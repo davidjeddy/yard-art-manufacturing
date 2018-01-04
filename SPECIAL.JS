2708a883a5c15e4895036c6204fc07197965b2b0
David J Eddy <me@davidjeddy.com>
2018-01-03 22:31:44 -0500

    INIT commit

---
+++ SPECIAL.JS
@@ -0,0 +1,11 @@
+/* author: Unregistered user */
+/* Creation date: 2/17/2002 */
+document.write('\
+<td rowspan="2" valign="top" align="center" width="160">\
+<br><br><br><br><br><br><br><br>\
+<font class="special">This Months Special:<br>\
+<a href="specials.html">\
+<font class="special2">Pattern Picnic Table</font></font>\
+<br>\
+<img src="iteams/tables/patterntable2-newview-mini.jpg" border="0" width="160" height="120" alt=""></a>\
+</td></tr>')
