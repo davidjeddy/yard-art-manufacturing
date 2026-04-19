db9fce69f3b1f1933860e367d6e62ff2481b30c4
David Eddy <me@davidjeddy.com>
2026-04-19 02:23:02 +0200

    chore: archive project — history cleared, redirecting to Codeberg
    This repository has been archived. The project has moved to:
    https://codeberg.org/DavidJEddy/yard-art-manufacturing

---
+++ footer.js
@@ -0,0 +1,7 @@
+document.write('<map name="yardartcomfooter">\
+	<area shape="rect" coords="30,05,140,95" href="contact.html">\
+	<area shape="rect" coords="140,05,250,95" href="policies.html">\
+	<area shape="rect" coords="250,05,380,95" href="terms.html">\
+</map>\
+<img src="interface/footer-wood3.jpg" border="0" width="452" height="100" alt="" usemap="#yardartcomfooter"><br><br>\
+<p align="center"><font>approximate measurements may vary <br>limited quantities at these prices<br>All materials copyright <a href="index.html">Yard Art Mfr . Com</a>, © 2002; or <a href="mailto:pheageygrean@hotmail.com">Pheagey Grean</a>, © 2002<br><br></font></p>')
---
+++ menu.js
@@ -0,0 +1,12 @@
+document.write('<map name="yardartcommenu">\
+	<area shape="rect" coords="0,50,140,85" href="planters.html">\
+	<AREA SHAPE="RECT" COORDS="0,85,140,120" HREF="decks.html">\
+	<AREA SHAPE="RECT" COORDS="0,120,140,157" HREF="windmills.html">\
+	<AREA SHAPE="RECT" COORDS="0,155,140,195" HREF="houses.html">\
+	<AREA SHAPE="RECT" COORDS="0,194,140,232" HREF="swings.html">\
+	<AREA SHAPE="RECT" COORDS="0,232,140,269" HREF="tables.html">\
+	<AREA SHAPE="RECT" COORDS="0,266,140,340" HREF="wells.html">\
+	<AREA SHAPE="RECT" COORDS="0,340,140,375" HREF="lighthouses.html">\
+</map>\
+<img src="interface/menu-wood3.jpg" border="0" alt="" usemap="#yardartcommenu">\
+')
